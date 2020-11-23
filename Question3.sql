19bbaa680396650b6f765c90ca0ed1511e76ed4e
PUdayKumarGit <74906030+PUdayKumarGit@users.noreply.github.com>
2020-11-23 22:22:51 +0530

    Update Question3.sql

--- Question3.sql
+++ Question3.sql
@@ -3,5 +3,5 @@ SELECT id,name,marks, CASE WHEN marks > 90 THEN "A+"
             WHEN marks > 50 THEN "B"
             WHEN marks >=40 THEN "C"
        ELSE
-           "Fail" END AS grade, price
+           "Fail" END AS grade
 FROM students;
